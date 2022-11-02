ab356a4db6daab83e22d16c52d979f07a07d26c2
Stephan109-5 <91522957+Stephan109-5@users.noreply.github.com>
2022-11-02 18:31:37 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,43 @@
+function to200px() {
+  document.getElementById("subject").style.width='200px';
+}
+
+function to500px() {
+  document.getElementById("subject").style.width='500px';
+}
+
+function to1px() {
+  document.getElementById("subject").style.border='1px solid black';
+  document.getElementById("subject").style.borderSpacing='1px';
+}
+
+function to10px() {
+  document.getElementById("subject").style.border='10px solid black';
+  document.getElementById("subject").style.borderSpacing='10px';
+}
+
+function to20px() {
+  document.getElementById("subject").style.border='20px solid black';
+  document.getElementById("subject").style.borderSpacing='20px';
+}
+
+function toGreen() {
+  document.getElementById("subject").style.backgroundColor='green';
+}
+
+function toGrey() {
+  document.getElementById("subject").style.backgroundColor='grey';
+}
+function toRed() {
+  document.getElementById("subject").style.backgroundColor='red';
+}
+function toBlue() {
+  document.getElementById("subject").style.backgroundColor='blue';
+}
+
+function reset() {
+  document.getElementById("subject").style.width='600px';
+  document.getElementById("subject").style.border='1px solid black';
+  document.getElementById("subject").style.borderSpacing='2px';
+  document.getElementById("subject").style.backgroundColor='yellow';
+}
