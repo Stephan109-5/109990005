bceb8991bc16dbfa5aa5243eacefb975ed005355
Stephan109-5 <91522957+Stephan109-5@users.noreply.github.com>
2022-11-25 21:23:07 +0800

    Update script.js

--- midterm/script.js
+++ midterm/script.js
@@ -1,4 +1,4 @@
-function component(name, x, y) {
+function component(name, x, y, ctx) {
     this.width;
     this.height;
     this.name = name;
@@ -6,13 +6,13 @@ function component(name, x, y) {
     this.y = y;
     this.speed_y = 30;
     this.color = "#" + randomLetter(colorlist);
-    this.rotate_flag = 0;
+    this.ctx = ctx;
+    this.rotate_flag = 0;   //0: 0 deg, 1: 90 deg, 2: 180 deg, 3: 270 deg
     this.old_flag = 0;
 
     //small adjustment to align shape to grid
     this.adjust = function(){
         if ((this.name == "o"||this.name == "l") && (this.y < 0) && (this.x == 135)){
-            // console.log(this.y);
             this.x += 15;
             return;
         }
@@ -22,6 +22,7 @@ function component(name, x, y) {
             return;
         }
 
+        //small adjustment after rotation
         if (this.old_flag != this.rotate_flag ){
             this.old_flag = this.rotate_flag;
             if (this.rotate_flag == 1|| this.rotate_flag == 0){
@@ -36,41 +37,42 @@ function component(name, x, y) {
         }
     }
 
+    //draw a red box around the shape if selected
     this.selected = function(){
-        ctx = myGameArea.context;
-        ctx.lineWidth = 1;
-        ctx.strokeStyle = 'red';
-        ctx.strokeRect(this.x - (0.5*this.width), this.y - (0.5*this.height), this.width, this.height);
-
+        var n_ctx = myGameArea.context;
+        n_ctx.lineWidth = 1;
+        n_ctx.strokeStyle = 'red';
+        n_ctx.strokeRect(this.x - (0.5*this.width), this.y - (0.5*this.height), this.width, this.height);
     }
 
     this.update = function(){
-        ctx = myGameArea.context
-        ctx.save();
-        ctx.translate(this.x, this.y);
-        ctx.rotate(this.rotate_flag * Math.PI / 2);
+        this.ctx.save();
+        this.ctx.translate(this.x, this.y);                 //move canvas coordinates to center of the would-be drawn shape
+        this.ctx.rotate(this.rotate_flag * Math.PI / 2); 
         this.adjust();
 
         switch (name){
             case "C":
                 this.width = 90;
                 this.height = 120;
+
+                //flip the dimensions when rotated
                 if (this.rotate_flag == 1 || this.rotate_flag == 3){
                     this.width = 120;
                     this.height = 90;
                 }
 
-                ctx.beginPath();
-                ctx.moveTo(45, -60);
-                ctx.lineTo(45, -60);
-                ctx.lineTo(45, -30);
-                ctx.lineTo(-15, -30);
-                ctx.lineTo(-15, 30);
-                ctx.lineTo(45, 30);
-                ctx.lineTo(45, 60);
-                ctx.lineTo(-45, 60);
-                ctx.lineTo(-45, -60);
-                ctx.closePath();
+                this.ctx.beginPath();
+                this.ctx.moveTo(45, -60);
+                this.ctx.lineTo(45, -60);
+                this.ctx.lineTo(45, -30);
+                this.ctx.lineTo(-15, -30);
+                this.ctx.lineTo(-15, 30);
+                this.ctx.lineTo(45, 30);
+                this.ctx.lineTo(45, 60);
+                this.ctx.lineTo(-45, 60);
+                this.ctx.lineTo(-45, -60);
+                this.ctx.closePath();
                 break;
                 
             case "S":
@@ -80,21 +82,21 @@ function component(name, x, y) {
                     this.width = 150;
                     this.height = 90;
                 }
-                ctx.beginPath();
-                ctx.moveTo(-45, -75);
-                ctx.lineTo(45, -75);
-                ctx.lineTo(45, -45);
-                ctx.lineTo(-15, -45);
-                ctx.lineTo(-15, -15);
-                ctx.lineTo(45, -15);
-                ctx.lineTo(45, 75);
-                ctx.lineTo(-45, 75);
-                ctx.lineTo(-45, 45);
-                ctx.lineTo(15, 45);
-                ctx.lineTo(15, 15);
-                ctx.lineTo(-45, 15);
-                ctx.lineTo(-45, -75);
-                ctx.closePath();
+                this.ctx.beginPath();
+                this.ctx.moveTo(-45, -75);
+                this.ctx.lineTo(45, -75);
+                this.ctx.lineTo(45, -45);
+                this.ctx.lineTo(-15, -45);
+                this.ctx.lineTo(-15, -15);
+                this.ctx.lineTo(45, -15);
+                this.ctx.lineTo(45, 75);
+                this.ctx.lineTo(-45, 75);
+                this.ctx.lineTo(-45, 45);
+                this.ctx.lineTo(15, 45);
+                this.ctx.lineTo(15, 15);
+                this.ctx.lineTo(-45, 15);
+                this.ctx.lineTo(-45, -75);
+                this.ctx.closePath();
                 
                 break;
 
@@ -105,13 +107,13 @@ function component(name, x, y) {
                     this.width = 120;
                     this.height = 30;
                 }
-                ctx.beginPath();
-                ctx.moveTo(-15, -60);
-                ctx.lineTo(15, -60);
-                ctx.lineTo(15, 60);
-                ctx.lineTo(-15, 60);
-                ctx.lineTo(-15, -60);
-                ctx.closePath();
+                this.ctx.beginPath();
+                this.ctx.moveTo(-15, -60);
+                this.ctx.lineTo(15, -60);
+                this.ctx.lineTo(15, 60);
+                this.ctx.lineTo(-15, 60);
+                this.ctx.lineTo(-15, -60);
+                this.ctx.closePath();
                 break;
 
             case "E":
@@ -122,36 +124,33 @@ function component(name, x, y) {
                     this.height = 90;
                 }
                 
-                ctx.beginPath();
-                ctx.moveTo(-45, -75);
-                ctx.lineTo(45, -75);
-                ctx.lineTo(45, -45);
-                ctx.lineTo(-15, -45);
-                ctx.lineTo(-15, -15);
-                ctx.lineTo(15, -15);
-                ctx.lineTo(15, 15);
-                ctx.lineTo(-15, 15);
-                ctx.lineTo(-15, 45);
-                ctx.lineTo(45, 45);
-                ctx.lineTo(45, 75);
-                ctx.lineTo(-45, 75);
-                ctx.lineTo(-45, -75);
-                ctx.closePath();
+                this.ctx.beginPath();
+                this.ctx.moveTo(-45, -75);
+                this.ctx.lineTo(45, -75);
+                this.ctx.lineTo(45, -45);
+                this.ctx.lineTo(-15, -45);
+                this.ctx.lineTo(-15, -15);
+                this.ctx.lineTo(15, -15);
+                this.ctx.lineTo(15, 15);
+                this.ctx.lineTo(-15, 15);
+                this.ctx.lineTo(-15, 45);
+                this.ctx.lineTo(45, 45);
+                this.ctx.lineTo(45, 75);
+                this.ctx.lineTo(-45, 75);
+                this.ctx.lineTo(-45, -75);
+                this.ctx.closePath();
                 break;
                 
             case "o":
-                ctx.beginPath();
-                ctx.moveTo(-30, -30);
-                ctx.lineTo(30, -30);
-                ctx.lineTo(30, 30);
-                ctx.lineTo(-30, 30);
-                ctx.lineTo(-30, -30);
-                ctx.closePath();
-                // ctx.fillRect(-30, -30, 60, 60);
-                
-                // ctx.strokeRect(-30, -30, 60, 60);
                 this.width = 60;
                 this.height = 60;
+                this.ctx.beginPath();
+                this.ctx.moveTo(-30, -30);
+                this.ctx.lineTo(30, -30);
+                this.ctx.lineTo(30, 30);
+                this.ctx.lineTo(-30, 30);
+                this.ctx.lineTo(-30, -30);
+                this.ctx.closePath();
                 break;
 
             case "t":
@@ -161,17 +160,17 @@ function component(name, x, y) {
                     this.width = 60;
                     this.height = 90;
                 }
-                ctx.beginPath();
-                ctx.moveTo(-15, 0);
-                ctx.lineTo(-15, -30);
-                ctx.lineTo(15, -30);
-                ctx.lineTo(15, 0);
-                ctx.lineTo(45, 0);
-                ctx.lineTo(45, 30);
-                ctx.lineTo(-45, 30);
-                ctx.lineTo(-45, 0);
-                ctx.lineTo(-15, 0);
-                ctx.closePath();
+                this.ctx.beginPath();
+                this.ctx.moveTo(-15, 0);
+                this.ctx.lineTo(-15, -30);
+                this.ctx.lineTo(15, -30);
+                this.ctx.lineTo(15, 0);
+                this.ctx.lineTo(45, 0);
+                this.ctx.lineTo(45, 30);
+                this.ctx.lineTo(-45, 30);
+                this.ctx.lineTo(-45, 0);
+                this.ctx.lineTo(-15, 0);
+                this.ctx.closePath();
                 break;
             
             case "z":
@@ -181,53 +180,50 @@ function component(name, x, y) {
                     this.width = 60;
                     this.height = 90;
                 }
-                ctx.beginPath();
-                ctx.moveTo(-45, -30);
-                ctx.lineTo(15, -30);
-                ctx.lineTo(15, 0);
-                ctx.lineTo(45, 0);
-                ctx.lineTo(45, 30);
-                ctx.lineTo(-15, 30);
-                ctx.lineTo(-15, 0);
-                ctx.lineTo(-45, 0);
-                ctx.lineTo(-45, -30);
-                ctx.closePath();
+                this.ctx.beginPath();
+                this.ctx.moveTo(-45, -30);
+                this.ctx.lineTo(15, -30);
+                this.ctx.lineTo(15, 0);
+                this.ctx.lineTo(45, 0);
+                this.ctx.lineTo(45, 30);
+                this.ctx.lineTo(-15, 30);
+                this.ctx.lineTo(-15, 0);
+                this.ctx.lineTo(-45, 0);
+                this.ctx.lineTo(-45, -30);
+                this.ctx.closePath();
                 break;
             
             case "l":
-                // ctx.translate(15, 0);
                 this.width = 60;
                 this.height = 90;
                 if (this.rotate_flag == 1 || this.rotate_flag == 3){
                     this.width = 90;
                     this.height = 60;
                 }
-                ctx.beginPath();
-                ctx.moveTo(-30, -45);
-                ctx.lineTo(30, -45);
-                ctx.lineTo(30, -15);
-                ctx.lineTo(0 , -15);
-                ctx.lineTo(0, 45);
-                ctx.lineTo(-30, 45);
-                ctx.lineTo(-30, -45);
-                ctx.closePath();
+                this.ctx.beginPath();
+                this.ctx.moveTo(-30, -45);
+                this.ctx.lineTo(30, -45);
+                this.ctx.lineTo(30, -15);
+                this.ctx.lineTo(0 , -15);
+                this.ctx.lineTo(0, 45);
+                this.ctx.lineTo(-30, 45);
+                this.ctx.lineTo(-30, -45);
+                this.ctx.closePath();
                 break;
         }
-        ctx.restore();
-        ctx.fillStyle = this.color;
-        ctx.fill();
-        ctx.fillStyle = null;
-        // ctx.lineWidth = 1.2;
-        // ctx.strokeStyle = 'black';
-        // ctx.stroke();
+
+        this.ctx.restore();
+        this.ctx.fillStyle = this.color;
+        this.ctx.fill();
+        this.ctx.fillStyle = null;
     }
-        
+    
+    //motion function
     this.newPos = function() {
         this.y += this.speed_y;
-        // if (this.speed_y == 0);
-        // this.speed_y = 30;
     }
 
+    //Detect if block is cliked
     this.clicked = function() {
         var myleft = this.x - (0.5 * this.width);
         var myright = this.x + (0.5 * this.width);
@@ -246,11 +242,11 @@ function component(name, x, y) {
 const colorlist = ["aa2e25", "a31545", "6d1b7b", "482880", "2c387e", "1769aa", "0276aa", "008394", "00695f", "357a38", 
 "618833", "8f9a27", "b2a429", "b28704", "b26a00", "b23c17"];
 
-//copied from somewhere
+//Randomiser function
 function randomLetter(arr){
     const randomIndex = Math.floor(Math.random() * arr.length);
-    // get random item
     const item = arr[randomIndex];
+
     return item;
 }
 
